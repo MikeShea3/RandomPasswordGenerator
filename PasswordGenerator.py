68bdfa2d9712cae2bbd7f8152809955658af41e4
MikeShea3 <60047589+MikeShea3@users.noreply.github.com>
2020-09-09 03:49:36 -0400

    Add files via upload

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,6 +1,9 @@
-# File: PasswordGenerator.py
+# Filename: PasswordGenerator.py
 # Author: Mike Shea
-# Description: A program to generate a password of a user-defined length
+# Description: PasswordGenerator.py generates a password from
+# upper and lower case chars with optional numbers and alternate characters.
+
+# Does not generate alt chars yet
 
 import random
 
@@ -10,10 +13,16 @@ alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
 
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
+altChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '`', '~', '{', '{', ']', '}', ';', ':',
+            ',', '<', '>', '.', '?', '/']
+
 generatedPassword = ""
 
-# User Input
+# USER INPUT
+# Length
 passwordLength = int(input("Enter the number of characters for the password: "))
+
+# Numbers
 useNumbers = input("Generate password with numbers? Y/N: ").lower()
 
 while useNumbers.lower() != 'y' and useNumbers.lower() != 'n':
@@ -24,33 +33,50 @@ if useNumbers == 'y':
 else:
     useNumbers = False
 
-# Generate Password
+# Alternate Characters
+useAltChars = input("Generate password with alternate characters? Y/N: ").lower()
+
+while useAltChars.lower() != 'y' and useAltChars.lower() != 'n':
+    useAltChars = input("Generate password with alternate characters? Y/N: ").lower()
+
+if useAltChars == 'y':
+    useAltChars = True
+else:
+    useAltChars = False
+
+# GENERATE PASSWORD
 for i in range(passwordLength):
 
     if useNumbers == False:
 
         randomInteger = random.randint(0,1)
 
-        # Upper Case Char
+        # Upper Case
         if randomInteger == 0:
             newChar = random.choice(alphabet)
+        # Lower Case
         elif randomInteger == 1:
             newChar = random.choice(alphabet).lower()
 
         generatedPassword += newChar
 
     else:
-        randomInteger = random.randint(0, 1)
+        randomInteger = random.randint(0, 2)
 
         #Generate Number
         if randomInteger == 0:
             newChar = random.choice(numbers)
 
-        # Generate Char
+        # Generate Lower Case
         elif randomInteger == 1:
+            newChar = random.choice(alphabet).lower()
+            
+        # Generate Upper Case
+        elif randomInteger == 2:
             newChar = random.choice(alphabet)
 
         generatedPassword += newChar
         
 # Output
 print("Generated password = " + generatedPassword)
+input()
